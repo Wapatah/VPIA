fc3e29c101a98ac9c6d16cfee393f4c982a8e087
Mordax <contact@mordax.io>
2019-10-16 14:27:42 -0400

    Remove and clean up uneccesary config files

--- config/db.js
+++ config/db.js
@@ -1,5 +1,15 @@
 // @Matterwiki - requiring the knexfile that contains our connection object.
-var config = require("../config/knexfile.js");
+var config = {
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "wapatah1",
+    database: "myapp_test"
+  },
+  useNullAsDefault: true
+};
+
 // @Matterwiki - imports knex with our connection object (found in knexfile).
 var knex = require("knex")(config);
 
@@ -8,3 +18,9 @@ module.exports = knex;
 
 // @Matterwiki - Run the latest DB migrations whenever the server starts.
 knex.migrate.latest([config]);
+
+// @Matterwiki - imports bookshelf along with our knex config.
+var bookshelf = require("bookshelf")(knex);
+
+// @Matterwiki - Exporting the bookshelf module for use in other files.
+module.exports = bookshelf;
