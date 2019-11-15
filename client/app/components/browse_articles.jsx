cd9aabc212fb50bd9b73b3fa3b5c37e9f9edb938
Yiyis <yolandayiyishao@gmail.com>
2019-11-15 10:37:19 -0500

    Adding carousel effects

--- client/app/components/browse_articles.jsx
+++ client/app/components/browse_articles.jsx
@@ -65,28 +65,94 @@ class BrowseArticles extends React.Component {
       );
     } else {
       return (
-        <div className="card-deck">
-          {this.state.articles.slice(0, 4).map(article => (
-            <div key={article.id} className="card card-block">
-              <img src="..." className="card-img-top" alt="..."></img>
-              <div className="card-body">
-                <p className="article-title">
-                  <Link to={"/article/" + article.id} className="text-dark">
-                    {article.title}
-                  </Link>
-                </p>
-                <p className="card-text"></p>
-                <p className="card-text">
-                  <small class="text-muted">
-                    <i className="fa fa-clock-o"></i>{" "}
-                    {new Date(
-                      article.updated_at.replace(" ", "T")
-                    ).toUTCString()}
-                  </small>
-                </p>
+        <div className="carousel slide" data-ride="carousel">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 text-right mb-4">
+                <a
+                  className="btn btn-outline-secondary prev"
+                  title="go back"
+                  data-slide="prev"
+                >
+                  <i className="fa fa-lg fa-chevron-left"></i>
+                </a>
+                <a
+                  className="btn btn-outline-secondary next"
+                  title="more"
+                  data-slide="next"
+                >
+                  <i className="fa fa-lg fa-chevron-right"></i>
+                </a>
               </div>
             </div>
-          ))}
+          </div>
+
+          <div className="container p-t-0 m-t-2 carousel-inner">
+            <div className="row row-equal carousel-item active m-t-0">
+              {this.state.articles.slice(0, 3).map(article => (
+                <div key={article.id} className="col-md-4">
+                  <div className="card">
+                    <img
+                      src="../assets/Placeholder.jpeg"
+                      className="card-img-top"
+                      alt="..."
+                    ></img>
+                    <div className="card-body">
+                      <p className="article-title">
+                        <Link
+                          to={"/article/" + article.id}
+                          className="text-dark"
+                        >
+                          {article.title}
+                        </Link>
+                      </p>
+                      <p className="card-text"></p>
+                      <p className="card-text">
+                        <small className="text-muted">
+                          <i className="fa fa-clock-o"></i>{" "}
+                          {new Date(
+                            article.updated_at.replace(" ", "T")
+                          ).toUTCString()}
+                        </small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="row row-equal carousel-item m-t-0">
+              {this.state.articles.slice(3, 8).map(article => (
+                <div key={article.id} className="col-md-4">
+                  <div className="card">
+                    <img
+                      src="../assets/Placeholder.jpeg"
+                      className="card-img-top"
+                      alt="..."
+                    ></img>
+                    <div className="card-body">
+                      <p className="article-title">
+                        <Link
+                          to={"/article/" + article.id}
+                          className="text-dark"
+                        >
+                          {article.title}
+                        </Link>
+                      </p>
+                      <p className="card-text"></p>
+                      <p className="card-text">
+                        <small className="text-muted">
+                          <i className="fa fa-clock-o"></i>{" "}
+                          {new Date(
+                            article.updated_at.replace(" ", "T")
+                          ).toUTCString()}
+                        </small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       );
     }
---
+++ client/assets/custom.js
@@ -0,0 +1,11 @@
+(function($) {
+  "use strict";
+  $(".next").click(function() {
+    $(".carousel").carousel("next");
+    return false;
+  });
+  $(".prev").click(function() {
+    $(".carousel").carousel("prev");
+    return false;
+  });
+})(jQuery);
