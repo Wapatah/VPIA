ea4f4a60096c3cb239ef48dcce484ce247db33d1
Yiyis <yolandayiyishao@gmail.com>
2019-12-02 17:00:16 -0500

    Edit page - added article nav

--- client/app/components/article.jsx
+++ client/app/components/article.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, hashHistory } from "react-router";
 import Loader from "./loader.jsx";
+import ArticleNav from "./article_nav.jsx";
 
 //import Alert from "react-s-alert";
 
