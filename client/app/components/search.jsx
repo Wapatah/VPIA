229dbad320c146602fd26cf79dc2c6907585a76d
Yiyi <414956851@qq.com>
2019-12-10 16:48:30 -0500

    search results - finished article cards

--- client/app/components/search.jsx
+++ client/app/components/search.jsx
@@ -71,53 +71,52 @@ class Search extends React.Component {
                 We found {this.state.articles.length} articles for your query
               </p>
             </div>
-            <div className="col-md-8">
-              {this.state.articles.length > 0 ? (
-                <div>
-                  {this.state.articles.map(article => (
-                    <div key={article.id} className="col-md-4">
-                      <div id="result" className="card card-block">
-                        <div
-                          id="my-card-img-top"
-                          dangerouslySetInnerHTML={{ __html: article.photo }}
-                        ></div>
-                        <div className="card-body">
-                          <p className="article-title">
-                            <Link
-                              to={"/article/" + article.id}
-                              className="text-dark"
-                            >
-                              {article.title}
-                            </Link>
-                          </p>
-                          <div className="card-text">
-                            <small class="text-muted">
-                              <p
-                                id="Baskerville"
-                                dangerouslySetInnerHTML={{
-                                  __html: article.culture_group
-                                }}
-                              ></p>
-                              <p id="Baskerville">
-                                <i className="fa fa-clock-o" />
-                                {new Date(
-                                  article.updated_at.replace(" ", "T")
-                                ).toUTCString()}
-                              </p>
-                            </small>
-                          </div>
+
+            {this.state.articles.length > 0 ? (
+              <div className="col-md-8 card-deck">
+                {this.state.articles.map(article => (
+                  <div key={article.id} className="col-md-4">
+                    <div id="result" className="card card-block">
+                      <div
+                        id="my-card-img-top"
+                        dangerouslySetInnerHTML={{ __html: article.photo }}
+                      ></div>
+                      <div className="card-body">
+                        <p className="article-title">
+                          <Link
+                            to={"/article/" + article.id}
+                            className="text-dark"
+                          >
+                            {article.title}
+                          </Link>
+                        </p>
+                        <div className="card-text">
+                          <small class="text-muted">
+                            <p
+                              id="Baskerville"
+                              dangerouslySetInnerHTML={{
+                                __html: article.culture_group
+                              }}
+                            ></p>
+                            <p id="Baskerville">
+                              <i className="fa fa-clock-o" />
+                              {new Date(
+                                article.updated_at.replace(" ", "T")
+                              ).toUTCString()}
+                            </p>
+                          </small>
                         </div>
                       </div>
                     </div>
-                  ))}
-                </div>
-              ) : (
-                <div className="no-results">
-                  <i className="fa fa-frown-o"></i>
-                  <p>Please try again with another query</p>
-                </div>
-              )}
-            </div>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className="no-results">
+                <i className="fa fa-frown-o"></i>
+                <p>Please try again with another query</p>
+              </div>
+            )}
           </div>
         </div>
       );
