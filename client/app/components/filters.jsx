3de818db21741ea539d24707357deb98ca77a572
Yiyis <yolandayiyishao@gmail.com>
2019-12-16 16:07:19 -0500

    Search page - adding filters

---
+++ client/app/components/filters.jsx
@@ -0,0 +1,183 @@
+import React from "react";
+import Loader from "./loader.jsx";
+import { hashHistory } from "react-router";
+//import Alert from "react-s-alert";
+
+class Filters extends React.Component {
+  constructor(props) {
+    super(props);
+    this.articleSelect = this.articleSelect.bind(this);
+    this.state = { articles: [], material: "", loading: true };
+  }
+
+  componentDidMount() {
+    var myHeaders = new Headers({
+      "Content-Type": "application/x-www-form-urlencoded",
+      "x-access-token": window.localStorage.getItem("userToken")
+    });
+    var myInit = { method: "GET", headers: myHeaders };
+    var that = this;
+    fetch("/api/articles/", myInit)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        if (response.error.error) {
+        } //Alert.error(response.error.message);
+        else {
+          that.setState({ articles: response.data });
+        }
+        that.setState({ loading: false });
+      });
+  }
+
+  articleSelect(material, e) {
+    e.preventDefault();
+    this.props.articleChange(this.state.material);
+  }
+
+  render() {
+    if (this.state.loading) return <Loader />;
+    return (
+      <div>
+        <div className="dropdown">
+          <button
+            className="btn btn-outline btn-block dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Culture Group
+          </button>
+
+          <div
+            className="dropdown-menu dropdown-menu-right dropdown-menu-lg-left"
+            aria-labelledby="dropdownMenuButton"
+          >
+            {this.state.articles.map(article => (
+              <a
+                key={article.culture_group}
+                href="#"
+                className="dropdown-item"
+                onClick={e => this.articleSelect(article.culture_group, e)}
+              >
+                <h4
+                  className="list-group-item-heading"
+                  dangerouslySetInnerHTML={{
+                    __html: article.culture_group
+                  }}
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <button
+            className="btn btn-outline btn-block dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Artwork Type
+          </button>
+
+          <div
+            className="dropdown-menu dropdown-menu-right dropdown-menu-lg-left"
+            aria-labelledby="dropdownMenuButton"
+          >
+            {this.state.articles.map(article => (
+              <a
+                key={article.artwork_type}
+                href="#"
+                className="dropdown-item"
+                onClick={e => this.articleSelect(article.artwork_type, e)}
+              >
+                <h4
+                  className="list-group-item-heading"
+                  dangerouslySetInnerHTML={{
+                    __html: article.artwork_type
+                  }}
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <button
+            className="btn btn-outline btn-block dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Material
+          </button>
+
+          <div
+            className="dropdown-menu dropdown-menu-right dropdown-menu-lg-left"
+            aria-labelledby="dropdownMenuButton"
+          >
+            {this.state.articles.map(article => (
+              <a
+                key={article.material}
+                href="#"
+                className="dropdown-item"
+                onClick={e => this.articleSelect(article.material, e)}
+              >
+                <h4
+                  className="list-group-item-heading"
+                  dangerouslySetInnerHTML={{
+                    __html: article.material
+                  }}
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <button
+            className="btn btn-outline btn-block dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Holding Institution
+          </button>
+
+          <div
+            className="dropdown-menu dropdown-menu-right dropdown-menu-lg-left"
+            aria-labelledby="dropdownMenuButton"
+          >
+            {this.state.articles.map(article => (
+              <a
+                key={article.institution}
+                href="#"
+                className="dropdown-item"
+                onClick={e => this.articleSelect(article.institution, e)}
+              >
+                <h4
+                  className="list-group-item-heading"
+                  dangerouslySetInnerHTML={{
+                    __html: article.institution
+                  }}
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Filters;
