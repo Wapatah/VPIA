703f6e6c5328a5d03af03ad36f14a954597ec8ce
Mordax <contact@mordax.io>
2020-02-19 13:31:08 -0500

    Jenkins - add deliver stage

---
+++ jenkins/scripts/deliver.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+echo 'The following "npm" command builds your Node.js/React application for'
+echo 'production in the local "build" directory (i.e. within the'
+echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
+echo 'correctly bundles React in production mode and optimizes the build for'
+echo 'the best performance.'
+set -x
+npm run start
+set +x
+
+echo 'Now...'
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
+echo 'created your initial Pipeline as a Jenkinsfile.)'
