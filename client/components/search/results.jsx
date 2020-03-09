735443d3a40735fac3fd5429cdcbe3676a5203ba
Yiyis <yolandayiyishao@gmail.com>
2020-03-09 13:52:29 -0400

    Artwork results - finished responsive layout

--- client/components/search/results.jsx
+++ client/components/search/results.jsx
@@ -37,7 +37,7 @@ class Results extends React.Component {
             <div id="left-side" className="col-md-1">
               <p className="text-right edit-page-title">Search Results</p>
               <p className="text-right">Filter by</p>
-              <div className="col-6 float-right">
+              <div className="col-lg-6 float-right">
                 <BrowseTopics topicChange={this.handleUpdate} />
                 <Filters topicChange={this.handleUpdate} />
               </div>
