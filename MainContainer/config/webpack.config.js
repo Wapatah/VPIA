13cfd2ac023d6a1c0bd7841cac50e7746d0e02c1
mordax <maya@mordax.io>
2020-05-27 10:26:02 -0400

    Fixing broken npm run dev by adding the microservices into the dev webpack

--- MainContainer/config/webpack.config.js
+++ MainContainer/config/webpack.config.js
@@ -10,6 +10,22 @@ const path = require("path");
 // @Matterwiki - TODO separate files for constants?
 const BUILD_DIR = path.resolve(__dirname, "../client/public");
 const APP_DIR = path.resolve(__dirname, "../client/components");
+const USER_SERVICE = path.resolve(
+  __dirname,
+  "../../UserService/client/components"
+);
+const HISTORY_SERVICE = path.resolve(
+  __dirname,
+  "../../HistoryService/client/components"
+);
+const SEARCH_SERVICE = path.resolve(
+  __dirname,
+  "../../SearchService/client/components"
+);
+const WIKI_SERVICE = path.resolve(
+  __dirname,
+  "../../WikiService/client/components"
+);
 
 module.exports = {
   mode: "development",
@@ -44,6 +60,54 @@ module.exports = {
       {
         test: /\.jsx?/,
         include: APP_DIR,
+        exclude: /node_modules/,
+        type: "javascript/auto",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.jsx?/,
+        include: USER_SERVICE,
+        exclude: /node_modules/,
+        type: "javascript/auto",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.jsx?/,
+        include: HISTORY_SERVICE,
+        exclude: /node_modules/,
+        type: "javascript/auto",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.jsx?/,
+        include: SEARCH_SERVICE,
+        exclude: /node_modules/,
+        type: "javascript/auto",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.jsx?/,
+        include: WIKI_SERVICE,
         exclude: /node_modules/,
         type: "javascript/auto",
         use: {
