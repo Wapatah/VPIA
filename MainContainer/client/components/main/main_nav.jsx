d8a6c6096cd420f0dd8f900f1bcde0eccc452dd2
Yiyi <414956851@qq.com>
2020-06-18 12:10:20 -0400

    Main container - finished main nav

--- MainContainer/client/components/main/main_nav.jsx
+++ MainContainer/client/components/main/main_nav.jsx
@@ -17,7 +17,7 @@ class MainNav extends React.Component {
       <div>
         <div id="mainheader" className="container-fluid">
           <div className="row">
-            <div id="mainlogo" className="col-lg-2">
+            <div id="mainlogo" className="col-lg-3">
               <Link to="landing">
                 <img
                   src="../assets/images/logo.png"
@@ -29,43 +29,45 @@ class MainNav extends React.Component {
               </Link>
             </div>
 
-            <div className="col-lg-8 col-lg-offset-2">
-              <div className="col-md-8">
-                <SearchForm
-                  onBlur={this.props.onBlur}
-                  onFocus={this.props.onFocus}
-                />
-              </div>
+            <div className="col-lg-6 col-lg-offset-3 align-self-end">
+              <div className="row">
+                <div className="main-search col-md-12">
+                  <SearchForm
+                    onBlur={this.props.onBlur}
+                    onFocus={this.props.onFocus}
+                  />
+                </div>
 
-              <nav className="navcontainer">
-                <div className="main-nav-item">
-                  <Link
-                    to="landing"
-                    activeClassName="active-yellow"
-                    aria-label="Go to home page"
-                  >
-                    <span>Home</span>
-                  </Link>
-                </div>
-                <div className="main-nav-item">
-                  <Link
-                    to="about"
-                    activeClassName="active-yellow"
-                    aria-label="Go to about page"
-                  >
-                    <span>About</span>
-                  </Link>
-                </div>
-                <div className="main-nav-item">
-                  <Link
-                    to="results"
-                    activeClassName="active-yellow"
-                    aria-label="Go to artwork page"
-                  >
-                    <span>Artwork</span>
-                  </Link>
-                </div>
-              </nav>
+                <nav className="row">
+                  <div className="col main-nav-item">
+                    <Link
+                      to="landing"
+                      activeClassName="active-yellow"
+                      aria-label="Go to home page"
+                    >
+                      <span>Home</span>
+                    </Link>
+                  </div>
+                  <div className="col main-nav-item">
+                    <Link
+                      to="about"
+                      activeClassName="active-yellow"
+                      aria-label="Go to about page"
+                    >
+                      <span>About</span>
+                    </Link>
+                  </div>
+                  <div className="col main-nav-item">
+                    <Link
+                      to="results"
+                      activeClassName="active-yellow"
+                      aria-label="Go to artwork page"
+                    >
+                      <span>Artwork</span>
+                    </Link>
+                  </div>
+                </nav>
+              </div>
             </div>
           </div>
         </div>
