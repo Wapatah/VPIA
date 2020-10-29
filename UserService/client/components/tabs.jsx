880738fed5a96d37079a89402ed34bf1e8297cc8
Yiyi <414956851@qq.com>
2020-10-29 17:09:27 -0400

    User - adjusted layout on sign up page

--- UserService/client/components/tabs.jsx
+++ UserService/client/components/tabs.jsx
@@ -24,7 +24,7 @@ class Tabs extends React.Component {
         return (
           <button
             onClick={this.select(i)}
-            className={`${active} btn btn-outline-secondary`}
+            className={`${active} btn btn-outline-secondary affliation-btn`}
           >
             {item}
           </button>
@@ -36,7 +36,7 @@ class Tabs extends React.Component {
   renderContent() {
     return React.Children.map(this.props.children, (item, i) => {
       if (i - 1 === this.state.active) {
-        return <div className="content">{item}</div>;
+        return <div className="content col-12">{item}</div>;
       } else {
         return;
       }
@@ -45,8 +45,15 @@ class Tabs extends React.Component {
 
   render() {
     return (
-      <div className="tabs">
-        {this.renderTabs()}
+      <div className="tabs row">
+        <div className="col-7">
+          <span className="text-danger">*</span> Do you have Affiliation with
+          Indigenous cultural heritage?{" "}
+          <small className="form-text text-muted help-text font-italic">
+            Example: Indigenous community, Indigenous culture group,…
+          </small>
+        </div>
+        <div className="col-5">{this.renderTabs()}</div>
         {this.renderContent()}
       </div>
     );
