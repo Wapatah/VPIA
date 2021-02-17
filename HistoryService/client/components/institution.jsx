82b6c1f03bd3ffea2596fc11764fdb2e82a96dd3
Yiyi <414956851@qq.com>
2021-02-17 16:23:36 -0500

    History - get the oldest edit history on Institution page

--- HistoryService/client/components/institution.jsx
+++ HistoryService/client/components/institution.jsx
@@ -12,7 +12,7 @@ class Institution extends React.Component {
   constructor(props) {
     super(props);
     this.deleteArticle = this.deleteArticle.bind(this);
-    this.state = { article: {}, user: {}, archive_id: "", loading: true };
+    this.state = { article: {}, user: {}, archives: {}, loading: true };
   }
 
   /* --------------------------------------------------------------------------------------------------------------------------------------------
@@ -26,21 +26,37 @@ class Institution extends React.Component {
 
     let myInit = { method: "GET", headers: myHeaders };
     let that = this;
+    let url =
+      `${process.env.HISTORYSERVICE}/api/articles/` +
+      that.props.params.articleId +
+      "/history";
 
-    fetch("/api/articles/" + that.props.params.articleId, myInit)
-      .then(function(response) {
-        return response.json();
+    Promise.all([
+      fetch("/api/articles/" + that.props.params.articleId, myInit),
+      fetch(url, myInit)
+    ])
+      .then(function(responses) {
+        // Get a JSON object from each of the responses
+        return Promise.all(
+          responses.map(function(response) {
+            return response.json();
+          })
+        );
       })
-      .then(function(response) {
-        if (response.error.error) {
-          StatusAlertService.showError(response.error.message);
-        } else {
-          that.setState({ article: response.data });
-        }
-        that.setState({ loading: false });
+      .then(function(data) {
+        const data1 = data[0];
+        const data2 = data[1];
+        that.setState({
+          article: data1.data,
+          archives: data2.data,
+          loading: false
+        });
+      })
+
+      .catch(function(error) {
+        StatusAlertService.showError(response.error.message);
       });
   }
-
   /* --------------------------------------------------------------------------------------------------------------------------------------------
   deleteArticle() - takes id, sends Delete request to Article and redirects
   user back to home.
@@ -77,13 +93,7 @@ class Institution extends React.Component {
   }
 
   // --------------------------------------------------------------------------------------------------------------------------------------------
-  // archiveUpdate() - Clicking on archive sets the archive id to update in browse_archives
-  archiveUpdate(id) {
-    this.setState({ archive_id: id });
-  }
-
-  // --------------------------------------------------------------------------------------------------------------------------------------------
-  // Renders the history page as well as the browse_archive and simple_article components.
+  // Renders the institution page.
   render() {
     let user_name = "";
     if (this.state.loading) return <Loader />;
@@ -92,19 +102,18 @@ class Institution extends React.Component {
         <div className="container-fluid">
           <StatusAlert />
           <div className="row">
-          <InfoBox
-            photo={this.state.article[0].photo}
-            photo_license={this.state.article[0].photo_license}
-            user_name={user_name}
-            institution={this.state.article[0].institution}
-            artwork_type={this.state.article[0].artwork_type}
-            culture_group={this.state.article[0].culture_group}
-            material={this.state.article[0].material}
-            tags={this.state.article[0].tags}
-            what_changed={this.state.article[0].what_changed}
-            delete={this.deleteArticle}
-          />
-
+            <InfoBox
+              photo={this.state.article[0].photo}
+              photo_license={this.state.article[0].photo_license}
+              user_name={user_name}
+              institution={this.state.article[0].institution}
+              artwork_type={this.state.article[0].artwork_type}
+              culture_group={this.state.article[0].culture_group}
+              material={this.state.article[0].material}
+              tags={this.state.article[0].tags}
+              what_changed={this.state.article[0].what_changed}
+              delete={this.deleteArticle}
+            />
             <div className="col-md-6 tabBar-content">
               <div className="tabBar row justify-content-between align-items-end">
                 <nav aria-label="breadcrumb col">
@@ -148,7 +157,7 @@ class Institution extends React.Component {
                     Institution
                   </Link>
                   <Link
-                    to={"/article/" + this.props.params.articleId}
+                    to={"/article/" + this.state.article[0].id}
                     className="bottom-align-text tabBar-tab vpia-tab"
                     aria-label="Artwork article tab, see the current published state of the article"
                   >
@@ -162,7 +171,15 @@ class Institution extends React.Component {
                     {this.state.article[0].title}-{" "}
                     {this.state.article[0].institution} Record
                   </h1>
-                  <div className="article-body"></div>
+
+                  <div
+                    className="single-article-body"
+                    dangerouslySetInnerHTML={{
+                      __html: this.state.archives[
+                        this.state.archives.length - 1
+                      ].body
+                    }}
+                  ></div>
                 </div>
               </div>
             </div>
