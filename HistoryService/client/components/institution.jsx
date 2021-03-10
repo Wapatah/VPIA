c39c0c6e87fe4afee034d37d0ebbaf5bb25f0a55
Mordax <contact@mordax.io>
2021-03-10 16:43:57 -0500

    Edit Archive/Institution Record completed - created an edit button that toggles the title and body edits for the original museum record, sending PUT request to the institution api endpoint, hiding edit inputs until toggled, removed unused variables, imported Tinymce editor

--- HistoryService/client/components/institution.jsx
+++ HistoryService/client/components/institution.jsx
@@ -6,11 +6,25 @@ import { Link } from "react-router";
 import Loader from "./helpers/loader.jsx";
 import InfoBox from "../../../WikiService/client/components/infobox.jsx";
 import StatusAlert, { StatusAlertService } from "react-status-alert";
+// Import Editor configurations - change general behaviour in the json, specific changes should be done in new.jsx and edit.jsx
+import Config from "../../config/editor.json";
+// TinyMCE Editor import
+import { Editor } from "@tinymce/tinymce-react";
 
 class Institution extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { article: {}, archives: {}, loading: true };
+    this.toggleEdit = this.toggleEdit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.state = {
+      article: {},
+      archives: {},
+      title: "",
+      body: "",
+      edit: true,
+      loading: true
+    };
   }
 
   /* --------------------------------------------------------------------------------------------------------------------------------------------
@@ -55,10 +69,60 @@ class Institution extends React.Component {
     }
   }
 
+  toggleEdit() {
+    this.setState({ edit: !this.state.edit });
+  }
+
+  onInputChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    let record = {
+      title: encodeURIComponent(this.state.title),
+      body: encodeURIComponent(this.state.body)
+    };
+
+    let headers = new Headers({
+      "Content-Type": "application/x-www-form-urlencoded",
+      "x-access-token": window.localStorage.getItem("userToken")
+    });
+
+    let that = this;
+
+    let request = {
+      method: "PUT",
+      headers: headers,
+      body:
+        "article_id=" +
+        that.props.params.articleId +
+        "&title=" +
+        record.title +
+        "&body=" +
+        record.body
+    };
+
+    let institution_url =
+      `${process.env.HISTORYSERVICE}/api/articles/` +
+      that.props.params.articleId +
+      "/history";
+
+    const res = await fetch(institution_url, request);
+    const json = await res.json();
+    if (json.error.error) {
+      StatusAlertService.showError(json.error.message);
+    } else {
+      StatusAlertService.showSuccess("Institution Record Updated Successfully");
+      this.setState({ edit: true });
+    }
+  }
+
   // --------------------------------------------------------------------------------------------------------------------------------------------
   // Renders the institution page.
   render() {
-    let user_name = "";
     let article = [],
       archive = [];
     if (this.state.loading) return <Loader />;
@@ -137,17 +201,79 @@ class Institution extends React.Component {
               </div>
             </div>
             <div class="tab-bar-card">
+              {window.localStorage.getItem("admin") === "1" ? (
+                <button
+                  className="btn btn-text float-right"
+                  type="button"
+                  aria-label="Edit Institution Record"
+                  onClick={this.toggleEdit}
+                >
+                  {this.state.edit ? (
+                    <i className="fa fa-edit"></i>
+                  ) : (
+                    <i className="fa fa-pencil"></i>
+                  )}
+                </button>
+              ) : (
+                ""
+              )}
               <div className="article-heading">
-                <h1 className="single-article-title">
-                  {archive.title}- {archive.institution} Record
-                </h1>
-
-                <div
-                  className="single-article-body"
-                  dangerouslySetInnerHTML={{
-                    __html: archive.body
-                  }}
-                ></div>
+                {this.state.edit ? (
+                  <h1 className="single-article-title">
+                    {archive.title}- {archive.institution} Record
+                  </h1>
+                ) : (
+                  <h1 className="single-article-title">
+                    <input
+                      name="title"
+                      value={this.state.title}
+                      onChange={this.onInputChange}
+                      className="form-control"
+                      placeholder="Change Institution title"
+                    />
+                    - {archive.institution} Record
+                  </h1>
+                )}
+                {this.state.edit ? (
+                  <div
+                    className="single-article-body"
+                    type={this.state.edit ? "password" : "text"}
+                    dangerouslySetInnerHTML={{
+                      __html: archive.body
+                    }}
+                  ></div>
+                ) : (
+                  <Editor
+                    initialValue={archive.body}
+                    init={{
+                      inline: false,
+                      menubar: false,
+                      automatic_uploads: true,
+                      images_upload_url: process.env.IMAGEUPLOAD,
+                      plugins: Config.plugins,
+                      toolbar: Config.toolbar,
+                      quickbars_insert_toolbar: false,
+                      quickbars_selection_toolbar: false,
+                      a11y_advanced_options: true,
+                      image_caption: true,
+                      images_reuse_filename: true,
+                      paste_data_images: true
+                    }}
+                    onChange={editor => {
+                      this.setState({ body: editor.level.content });
+                    }}
+                  />
+                )}
+                {this.state.edit ? (
+                  ""
+                ) : (
+                  <button
+                    className="btn btn-default btn-block btn-lg"
+                    onClick={this.handleSubmit}
+                  >
+                    Update Institution Record
+                  </button>
+                )}
               </div>
             </div>
           </div>
